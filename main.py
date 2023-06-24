7fcc98c1c07a67716e61b621a88d9bd764bde511
Shizuka <tkpp26@gmail.com>
2023-06-23 23:17:02 -0700

    Edit main.py to display substrings

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
     ##################################################
-    # Comlete your code here
+    # Complete your code here
     ##################################################
-    pass
+    original_str = "Python Programming"
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:18] # or [7:-1?]
+    merged_str = sub2 + " " + sub1
+
+    print(sub2)
+    print(sub1)
+    print(merged_str)
+    pass #what is this for
 
 
 if __name__ == '__main__':
