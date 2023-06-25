8f71c752a1c1a32467545ccc94d9e7d20815e6aa
Shizuka <tkpp26@gmail.com>
2023-06-24 17:35:53 -0700

    Edit program and run test again to ensure the code is correct

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ def main():
     ##################################################
     original_str = "Python Programming"
     sub1 = original_str[0:6]
-    sub2 = original_str[7:18] # or [7:-1?]
+    sub2 = original_str[7:18] # or sub2 = original_str[7:]
     merged_str = sub2 + " " + sub1
 
     print(sub2)
     print(sub1)
     print(merged_str)
-    pass #what is this for
-
 
 if __name__ == '__main__':
     main()
